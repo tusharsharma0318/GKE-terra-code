18e3f5e26fda932e6fe9902ea0a89bb923acbc31
Tushar Sharma <tusharsharma0318@gmail.com>
2026-06-21 22:38:01 +0000

    corrected the format

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project     = "project-f58454be-54a8-4e89-a39"
-  region      = "us-east4"
-  zone        = "us-east4-a"
+  project = "project-f58454be-54a8-4e89-a39"
+  region  = "us-east4"
+  zone    = "us-east4-a"
 }
